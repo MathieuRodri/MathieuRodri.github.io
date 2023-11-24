ff7d4b48fc0c19ac4d1780349a842924a3e66b6a
Mathieu RODRIGUES <74418584+MathieuRodri@users.noreply.github.com>
2023-11-24 19:26:21 +0100

    Update index.30e394f0.js

--- index.30e394f0.js
+++ index.30e394f0.js
@@ -1,6 +1,6 @@
 let copyright = document.getElementById('copyright');
 copyright.innerHTML += '&copy;';
-copyright.innerHTML += ' 2022 - ';
+copyright.innerHTML += ' 2023 - ';
 copyright.innerHTML += new Date().getFullYear();
 copyright.innerHTML += ' mathieurodri.github.io - All Rights Reserved.';
 copyright.innerHTML += '<br/>Last Updated : ';
